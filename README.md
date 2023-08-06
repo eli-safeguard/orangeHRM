99353eb34316435fbf0fe1b34b69cfed606f10c2
Eli Bonner <eli.bonner@proton.me>
2023-08-06 15:33:15 -0500

    finished logout test and some refactoring

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 README
 
+## Setup
 1. If necessary, install an IDE like VS Code
 2. If necessary, install NodeJS version 18
-3. Clone the repository from: _____
+3. Clone the repository
 4. Open the project in your IDE 
 5. In a terminal in your IDE, run "npm init playwright@latest"
     - answer questions: use Typescript, rest accept the default answer
 6. Next, "npm install"
+
+## Running tests
+1. Run "npm test" (for headless execution)
+2. To watch tests run step-by-step, "npm test -- --debug"
--- tests/recruitment.spec.ts
+++ tests/recruitment.spec.ts
@@ -6,54 +6,44 @@ let basePage: BasePage;
 test.beforeEach(async ({ page }) => {
 	basePage = new BasePage(page);
 	await page.goto('https://opensource-demo.orangehrmlive.com/');
-});
-
-
-test('not logged in', async ({ page }) => {
-  console.log('current page: ' + page.url());
-  // Expect url not to contain "dashboard"
-  await expect(page).not.toHaveURL(/dashboard/);
-});
-
-test('login', async ({ page }) => {
 	await basePage.login();
 });
 
-test('navigate to recruitment page', async ({ page }) => {
-	await basePage.login();
-	await basePage.getToRecruitment();
-});
 
 // READ
 test('validate candidate on recruitment page', async () => {
-	await basePage.login();
 	await basePage.getToRecruitment();
 	await basePage.verifyCandidateAppears('Chris Harris');
 });
 
 // CREATE
 test('create candidate on recruitment page', async () => {
-	await basePage.login();
 	await basePage.getToRecruitment();
-	await basePage.addCandidate('test3', 'person3', 'b3@c3.com');
+	await basePage.addCandidate('test', 'person', 'a@b.com');
 	await basePage.getToRecruitment();
-	await basePage.verifyCandidateAppears('test3 person3');
+	await basePage.verifyCandidateAppears('test person');
 });
 
 // UPDATE
-test('update candidate on recruitment page', async ({ page }) => {
-	await page.waitForTimeout(5000);
-	await basePage.login();
+test('update candidate on recruitment page', async () => {
 	await basePage.getToRecruitment();
-	await basePage.updateCandidate('test3', 'person3', 'test2', 'person2');
+	await basePage.updateCandidate('test', 'person', 'test2', 'person2');
 	await basePage.getToRecruitment();
 	await basePage.verifyCandidateAppears('test2 person2');
 });
 
 // DELETE
-test('delete candidate on recruitment page', async ({ page }) => {
-	await basePage.login();
+test('delete candidate on recruitment page', async () => {
 	await basePage.getToRecruitment();
-	await basePage.deleteCandidate('test3', 'person3');
-	await basePage.verifyCandidateAppears('test3 person3', false);
+	await basePage.deleteCandidate('test2', 'person2');
+	await basePage.verifyCandidateAppears('test2 person2', false);
+});
+
+test('log out', async ({ page }) => {
+	await page.locator('span[class*="userdropdown"]').click();
+	await page.getByRole('menuitem', { name: 'Logout' }).click();
+
+	// Expect url not to contain "dashboard"
+	await expect(page).not.toHaveURL(/dashboard/);
+	await expect(page).toHaveURL(/auth\/login/);
 });
